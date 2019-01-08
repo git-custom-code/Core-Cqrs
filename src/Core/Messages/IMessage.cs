09c1994c401b3bcd8a165895d2eef0faecceb620
Stefan Loeffler <s.loeffler@custom-co.de>
2019-01-08 14:28:14 +0100

    added contracts for messages

---
+++ src/Core/Messages/IMessage.cs
@@ -0,0 +1,8 @@
+namespace CustomCode.Core.Messages
+{
+    /// <summary>
+    /// Marker interfaces that should be implemented by all messages.
+    /// </summary>
+    public interface IMessage
+    { }
+}
