2e80c4abd928f742561dbaf3f8f0ed453f3f5a24
Stefan Loeffler <s.loeffler@custom-co.de>
2019-01-08 14:30:45 +0100

    added contracts for message serialization

---
+++ src/Core/Messages.Serialization/IMessageDeserializer.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomCode.Core.Messages.Serialization
+{
+    /// <summary>
+    /// Interface for types that can deserialize an <see cref="IMessage"/> from a binary array.
+    /// </summary>
+    public interface IMessageDeserializer
+    {
+        /// <summary>
+        /// Deserialize a message of type <typeparamref name="T"/> from a binary array.
+        /// </summary>
+        /// <typeparam name="T"> The type of the message to be deserialized. </typeparam>
+        /// <param name="serializedMessage"> The serialized message to be deserialized. </param>
+        /// <returns> The deserialized message. </returns>
+        T Deserialize<T>(byte[] serializedMessage) where T : IMessage;
+    }
+}
---
+++ src/Core/Messages.Serialization/IMessageSerializer.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomCode.Core.Messages.Serialization
+{
+    /// <summary>
+    /// Interface for types that can serialize an <see cref="IMessage"/> to a binary array.
+    /// </summary>
+    public interface IMessageSerializer
+    {
+        /// <summary>
+        /// Serialize the specified <paramref name="message"/> to a binary array.
+        /// </summary>
+        /// <typeparam name="T"> The type of the message to be serialized. </typeparam>
+        /// <param name="message"> The message to be serialized. </param>
+        /// <returns> The binary array that contains the serialized <paramref name="message"/>. </returns>
+        byte[] Serialize<T>(T message) where T : IMessage;
+    }
+}
