6ca5c96c41e2e5659419ddff1acf2d6a7ff0a6a7
Stefan Loeffler <s.loeffler@custom-co.de>
2019-01-08 14:33:59 +0100

    added protocol buffer message serialization

---
+++ src/Core/Messages.Serialization.ProtocolBuffer/MessageDeserializer.cs
@@ -0,0 +1,44 @@
+﻿namespace CustomCode.Core.Messages.Serialization.ProtocolBuffer
+{
+    using ProtoBuf.Meta;
+    using System.IO;
+
+    /// <summary>
+    /// An <see cref="IMessageDeserializer"/> implementation using google's protocol buffer.
+    /// </summary
+    public sealed class MessageDeserializer : IMessageDeserializer
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="MessageDeserializer"/> type.
+        /// </summary>
+        /// <param name="model">
+        /// Specify a custom <see cref="RuntimeTypeModel"/> if you need to change protocol buffer's default serialization rules.
+        /// </param>
+        public MessageDeserializer(RuntimeTypeModel model = null)
+        {
+            Model = model ?? RuntimeTypeModel.Default;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="RuntimeTypeModel"/> that specifies protocol buffer's serialization rules.
+        /// </summary>
+        private RuntimeTypeModel Model { get; }
+
+        #endregion
+
+        #region Logic
+
+        /// <inheritdoc />
+        public T Deserialize<T>(byte[] data) where T : IMessage
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                return (T)Model.Deserialize(stream, null, typeof(T));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Core/Messages.Serialization.ProtocolBuffer/MessageSerializer.cs
@@ -0,0 +1,45 @@
+﻿namespace CustomCode.Core.Messages.Serialization.ProtocolBuffer
+{
+    using ProtoBuf.Meta;
+    using System.IO;
+
+    /// <summary>
+    /// An <see cref="IMessageSerializer"/> implementation using google's protocol buffer.
+    /// </summary>
+    public sealed class MessageSerializer : IMessageSerializer
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="MessageSerializer"/> type.
+        /// </summary>
+        /// <param name="model">
+        /// Specify a custom <see cref="RuntimeTypeModel"/> if you need to change protocol buffer's default serialization rules.
+        /// </param>
+        public MessageSerializer(RuntimeTypeModel model = null)
+        {
+            Model = model ?? RuntimeTypeModel.Default;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="RuntimeTypeModel"/> that specifies protocol buffer's serialization rules.
+        /// </summary>
+        private RuntimeTypeModel Model { get; }
+
+        #endregion
+
+        #region Logic
+
+        /// <inheritdoc />
+        public byte[] Serialize<T>(T message) where T : IMessage
+        {
+            using (var stream = new MemoryStream())
+            {
+                Model.Serialize(stream, message);
+                return stream.ToArray();
+            }
+        }
+
+        #endregion
+    }
+}
