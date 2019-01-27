129028b6a958f43df02f96ec142b8abb31f881f3
Stefan Loeffler <s.loeffler@custom-co.de>
2019-01-27 21:48:44 +0100

    added strongly typed netmq protocol enumeration

---
+++ src/Core/Messages.Transport.NetMQ/Addresses/Protocol.cs
@@ -0,0 +1,16 @@
+namespace CustomCode.Core.Messages.Transport.NetMQ
+{
+    /// <summary>
+    /// Enumeration that defines NetMQ's supported tranport protocols.
+    /// </summary>
+    /// <remarks> See https://netmq.readthedocs.io/en/latest/transports/ for more details. </remarks>
+    public enum Protocol : byte
+    {
+        /// <summary> Transmission control protocol (see https://en.wikipedia.org/wiki/Transmission_Control_Protocol). </summary>
+        Tcp = 0,
+        /// <summary> In-Process protocol (see http://api.zeromq.org/2-1:zmq-inproc). </summary>
+        InProc = 1,
+        /// <summary> Pragmatic general multicast (see https://en.wikipedia.org/wiki/Pragmatic_General_Multicast). </summary>
+        Pgm = 2
+    }
+}
