a375fbd67c431f5eb8f3aa890b1932d3ba24a80f
Stefan Loeffler <s.loeffler@custom-co.de>
2019-02-03 11:13:12 +0100

    added in-process endpoint implementation

---
+++ src/Core/Messages.Transport.NetMQ/Endpoints/InProcessEndpoint.cs
@@ -0,0 +1,60 @@
+namespace CustomCode.Core.Messages.Transport.NetMQ.Endpoints
+{
+    using global::NetMQ;
+
+    /// <summary>
+    /// An <see cref="IEndpoint"/> implementation that uses <see cref="NetMQSocket"/> to send and/or receive
+    /// <see cref="IMessage"/> instances using in-process communication protocl. 
+    /// </summary>
+    public sealed class InProcessEndpoint : ISocketEndpoint
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="InProcessEndpoint"/> type.
+        /// </summary>
+        /// <param name="identity"> The endpoint's unique identity. </param>
+        public InProcessEndpoint(string identity)
+            : this(new Identity<string>(identity))
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="InProcessEndpoint"/> type.
+        /// </summary>
+        /// <param name="identity"> The endpoint's unique identity. </param>
+        public InProcessEndpoint(Identity<string> identity)
+        {
+            Identity = identity;
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <inheritdoc />
+        public Protocol Protocol { get; } = Protocol.InProc;
+
+        /// <summary>
+        /// Gets the endpoint's identity.
+        /// </summary>
+        public Identity<string> Identity { get; }
+
+        #endregion
+
+        #region Logic
+
+        /// <inheritdoc />
+        public string AsString()
+        {
+            return $"inproc://{Identity}";
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return AsString();
+        }
+
+        #endregion
+    }
+}
