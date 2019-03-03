3ad5b3e0bc7cb0912785473d55192247c1eea3ce
Stefan Loeffler <s.loeffler@custom-co.de>
2019-03-03 09:40:06 +0100

    added command marker interface

---
+++ src/Core/Messages/ICommand.cs
@@ -0,0 +1,8 @@
+namespace CustomCode.Core.Messages
+{
+    /// <summary>
+    /// Marker interfaces that should be implemented by all commands.
+    /// </summary>
+    public interface ICommand : IMessage
+    { }
+}
