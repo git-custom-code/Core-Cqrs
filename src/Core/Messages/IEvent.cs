84afa4812a017ff4c14de07d57d4dddded430c53
Stefan Loeffler <s.loeffler@custom-co.de>
2019-03-04 19:21:00 +0100

    added event marker interface

---
+++ src/Core/Messages/IEvent.cs
@@ -0,0 +1,8 @@
+namespace CustomCode.Core.Messages
+{
+    /// <summary>
+    /// Marker interfaces that should be implemented by all events.
+    /// </summary>
+    public interface IEvent : IMessage
+    { }
+}
