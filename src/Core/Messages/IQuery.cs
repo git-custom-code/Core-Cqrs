0af6d753bc5c89fb3582699235622690963b3dda
Stefan Loeffler <s.loeffler@custom-co.de>
2019-03-06 23:07:38 +0100

    added marker interface for queries

---
+++ src/Core/Messages/IQuery.cs
@@ -0,0 +1,8 @@
+namespace CustomCode.Core.Messages
+{
+    /// <summary>
+    /// Marker interfaces that should be implemented by all queries.
+    /// </summary>
+    public interface IQuery : IMessage
+    { }
+}
